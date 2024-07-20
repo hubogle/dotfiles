f3e417e6732731a170bcbe815ae5b669410427f0
hubogle <hubogle@gmail.com>
2024-07-20 20:17:25 +0800

    feat(vim): key mappings

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -6,10 +6,21 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "q", function()
+	require("nvchad.tabufline").close_buffer()
+end, { desc = "buffer close" })
+
+map("n", "<C-b>p", ":Telescope oldfiles<CR>", { noremap = true, silent = true, desc = "Open recently opened files" })
+map(
+	"i",
+	"<C-b>p",
+	"<ESC>:Telescope oldfiles<CR>",
+	{ noremap = true, silent = true, desc = "Open recently opened files" }
+)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 -- format
 map("n", "<leader>fm", function()
-  require("conform").format()
+	require("conform").format()
 end)
--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -3,24 +3,40 @@ require("nvchad.options")
 -- add yours here!
 
 local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
 
-o.relativenumber = true -- 相对坐标
+-- 设置显示 tab 和空格字符
+o.list = true
+o.listchars = "tab:» ,trail:·,extends:>,precedes:<,space: "
+
+-- 行号显示
+o.number = true -- 显示行号
+-- o.relativenumber = true -- 相对行号
 
 -- Indenting
-o.expandtab = false -- 将制表符（Tab）转换为空格
+o.expandtab = true -- 将制表符（Tab）转换为空格
 o.shiftwidth = 4 -- 自动缩进时使用的空格数量
 o.smartindent = true -- 智能缩进
 o.tabstop = 4 -- 设置 Tab 字符的宽度为 4 个空格
 o.softtabstop = 4 -- 设置软 Tab 的宽度为 4 个空格
 
+-- 搜索
 o.ignorecase = true -- 搜索时忽略大小写
 o.smartcase = true -- 启用智能大小写匹配
-o.signcolumn = "yes" -- 始终显示标志列
+o.hlsearch = true -- 搜索时高亮匹配项
+o.incsearch = true -- 搜索时增量高亮
+
+-- 窗口分割
 o.splitbelow = true -- 设置新分屏在当前窗口的下方
 o.splitright = true -- 设置新分屏在当前窗口的右侧
 
-o.cursorlineopt = "both" -- 光标线突出显示
+-- 光标
+o.cursorline = true -- 启用光标行
+-- o.cursorcolumn = true -- 启用光标列
+o.cursorlineopt = "both" -- 光标行突出显示
 
 -- setting
 o.encoding = "utf-8"
+o.backup = false -- 禁用备份文件
+o.writebackup = false -- 禁用写入备份文件
+o.swapfile = false -- 禁用交换文件
+o.undofile = true -- 启用持久撤销
