a64789a170f218df17b8c98f26d8ca376d42da23
hubogle <hubogle@gmail.com>
2024-07-28 14:42:58 +0800

    fix: vim config

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -15,7 +15,7 @@ o.number = true -- 显示行号
 
 -- Indenting
 o.expandtab = true -- 将制表符（Tab）转换为空格
-o.shiftwidth = 4 -- 自动缩进时使用的空格数量
+o.shiftwidth = 2 -- 自动缩进时使用的空格数量
 o.smartindent = true -- 智能缩进
 o.tabstop = 4 -- 设置 Tab 字符的宽度为 4 个空格
 o.softtabstop = 4 -- 设置软 Tab 的宽度为 4 个空格
