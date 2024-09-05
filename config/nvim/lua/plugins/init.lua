c17692c59ef6e6be6e28c6583aa6180dfe5a1ab4
hubogle <hubogle@gmail.com>
2024-09-05 17:20:20 +0800

    feat: vim update treesitter

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,52 +1,52 @@
 return {
 	{
 		"stevearc/conform.nvim",
-		-- event = 'BufWritePre', -- uncomment for format on save
 		config = function()
 			require("configs.conform")
 		end,
 	},
 
-	-- These are some examples, uncomment them if you want to see them work!
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			-- require("nvchad.configs.lspconfig").defaults()
 			require("configs.lspconfig")
 		end,
 	},
-	--
-	-- {
-	-- 	"williamboman/mason.nvim",
-	-- 	opts = {
-	-- 		ensure_installed = {
-	-- 			"lua-language-server", "stylua",
-	-- 			"html-lsp", "css-lsp" , "prettier"
-	-- 		},
-	-- 	},
-	-- },
-	--
-	-- 语法高亮插件
+
+	{
+		"nvim-telescope/telescope.nvim",
+		opts = function()
+			local conf = require("nvchad.configs.telescope")
+			conf.defaults.file_ignore_patterns = { "%.git/" }
+			conf.defaults.mappings.i = {
+				["<C-j>"] = require("telescope.actions").move_selection_next,
+				["<C-k>"] = require("telescope.actions").move_selection_previous,
+				["<Esc>"] = require("telescope.actions").close,
+			}
+			return conf
+		end,
+	},
+
+	-- formatting , linting
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("configs.conform")
+		end,
+	},
+
+	-- 语法高亮
 	{
 		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"vim",
-				"lua",
-				"vimdoc",
-				"html",
-				"css",
-				"go",
-				"python",
-				"rust",
-				"markdown",
-				"dockerfile",
-				"gitignore",
-				"sql",
-				"yaml",
-				"toml",
-			},
-		},
+		config = function()
+			require("configs.treesitter")
+		end,
+	},
+
+	-- 目录树
+	{
+		"nvim-tree/nvim-tree.lua",
+		lazy = false,
 	},
 
 	-- indent lines
@@ -54,29 +54,9 @@ return {
 		"lukas-reineke/indent-blankline.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
-			-- require("nvchad.configs.blankline").defaults()
 			require("configs.blankline")
 		end,
 	},
-
-	-- formatting , linting
-	{
-		"stevearc/conform.nvim",
-		lazy = true,
-		config = function()
-			require("configs.conform")
-		end,
-	},
-
-	-- dashboard
-	-- {
-	-- 	"nvimdev/dashboard-nvim",
-	-- 	event = "VimEnter",
-	-- 	config = function()
-	-- 		require("configs.dashboard")
-	-- 	end,
-	-- 	dependencies = { { "nvim-tree/nvim-web-devicons" } },
-	-- },
 
 	-- https://github.com/kdheepak/lazygit.nvim
 	-- git 功能插件
@@ -114,11 +94,6 @@ return {
 			{ "nvim-telescope/telescope.nvim" },
 			{ "Shatur/neovim-session-manager" },
 		},
-	},
-	-- 目录树
-	{
-		"nvim-tree/nvim-tree.lua",
-		lazy = false,
 	},
 
 	-- markdown
