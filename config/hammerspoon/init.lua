a5b328a2433578a8695d38409720be5e85c42971
hubogle <hubogle@gmail.com>
2024-09-12 20:16:17 +0800

    update: hammerspoon input

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,26 +1,25 @@
-hotkey = require "hs.hotkey"
-hyperkey = { '⌘⌃⇧' }
-hyper = { '⌃⌥' }
-Hyper = { '⌘⌃⌥' }
-keymap = { '⌃' }
+hotkey = require("hs.hotkey")
+hyperkey = { "⌘⌃⇧" }
+hyper = { "⌃⌥" }
+Hyper = { "⌘⌃⌥" }
+keymap = { "⌃" }
 
 -- 设置警报风格
 hs.alert.defaultStyle.strokeColor = { white = 1, alpha = 0 }
 hs.alert.defaultStyle.fillColor = { white = 0.05, alpha = 0.75 }
 hs.alert.defaultStyle.radius = 10
 
-
 local module_list = {
-    -- "modules/reload",       -- 自动加载
-    "modules/input", -- 切换输入法
-    "modules/doubleCmd", -- 两次 Q 退出
-    "modules/app", -- App 快捷键
-    "modules/windows", -- 窗口管理
-    "modules/moveWindows", -- 移动窗口
-    -- "modules/airpods",      -- 链接 AirPods 快捷键
+	-- "modules/reload",       -- 自动加载
+	"modules/input", -- 切换输入法
+	"modules/doubleCmd", -- 两次 Q 退出
+	"modules/app", -- App 快捷键
+	"modules/windows", -- 窗口管理
+	"modules/moveWindows", -- 移动窗口
+	-- "modules/airpods",      -- 链接 AirPods 快捷键
 }
 for _, v in ipairs(module_list) do
-    require(v)
+	require(v)
 end
 
 -- https://github.com/hououinkami/HammerspoonConfig
@@ -29,44 +28,3 @@ end
 -- 最大化：⌃ + ⌥ + return
 -- 全屏 / 非全屏：⌃ + ⌥ + ⌘ + return
 -- 回到初始状态：⌃ + ⌥ + delete
-
-local function pressFn(mods, key)
-    if key == nil then
-        key = mods
-        mods = {}
-    end
-    return function() hs.eventtap.keyStroke(mods, key, 1000) end
-end
-
--- 为不同的按键绑定创建快捷方式
-local hotkeys = {
-    hs.hotkey.new({'ctrl'}, 'h', pressFn('left'), nil, pressFn('left')),
-    hs.hotkey.new({'ctrl'}, 'j', pressFn('down'), nil, pressFn('down')),
-    hs.hotkey.new({'ctrl'}, 'k', pressFn('up'), nil, pressFn('up')),
-    hs.hotkey.new({'ctrl'}, 'l', pressFn('right'), nil, pressFn('right'))
-}
-
-local function enableHotkeys()
-    for _, hk in ipairs(hotkeys) do
-        hk:enable()
-    end
-end
-
-local function disableHotkeys()
-    for _, hk in ipairs(hotkeys) do
-        hk:disable()
-    end
-end
-
--- 创建一个应用程序观察者
-appWatcher = hs.application.watcher.new(function(appName, eventType, app)
-    if appName == "Alacritty" then
-        if eventType == hs.application.watcher.activated then
-            disableHotkeys()
-        elseif eventType == hs.application.watcher.deactivated then
-            enableHotkeys()
-        end
-    end
-end)
-
-appWatcher:start()
--- config/hammerspoon/modules/input.lua
+++ config/hammerspoon/modules/input.lua
@@ -1,57 +1,40 @@
 -- 设置App对应的输入法
 local App2Ime = {
-  ["/Applications/WeChat.app"] = "Chinese",
-  ["/Applications/企业微信.app"] = "Chinese",
-  ["/Applications/Telegram.app"] = "Chinese",
-  ["/Applications/Preview.app"] = "Chinese",
-  ["/Applications/Microsoft Word.app"] = "Chinese",
-  ["/Applications/Microsoft Excel.app"] = "Chinese",
-  ["/Applications/Microsoft PowerPoint.app"] = "Chinese",
-  ["/Applications/OpenCat.app"] = "Chinese",
-  ["/System/Applications/Music.app"] = "English",
-  ["/System/Library/CoreServices/Finder.app"] = "English",
-  ["/Applications/Safari.app"] = "English",
-  ["/Applications/Google Chrome.app"] = "English",
-  ["/Applications/Microsoft Edge.app"] = "English",
-  ["/Applications/DataGrip.app"] = "English",
-  ["/Applications/GoLand.app"] = "English",
-  ["/Applications/VimR.app"] = "English",
-  ["/Applications/Paw.app"] = "English",
-  ["/Applications/Utilities/Terminal.app"] = "English",
-  ["/System/Library/CoreServices/Spotlight.app"] = "English",
-  ["/Applications/Hammerspoon.app"] = "English",
-  ["/Applications/Visual Studio Code.app"] = "English",
-  ["/Applications/System Preferences.app"] = "English",
-  ["/Applications/Alacritty.app"] = "English",
-  ["/Applications/Sublime Text.app"] = "English",
+	["org.hammerspoon.Hammerspoon"] = "English",
+	["org.alacritty"] = "English",
+	["com.apple.Terminal"] = "English",
+	["com.microsoft.VSCode"] = "English",
+	["com.apple.Safari"] = "English",
+	["com.microsoft.edgemac"] = "English",
+	["com.google.Chrome"] = "English",
+	["com.apple.systempreferences"] = "English",
+	["com.apple.finder"] = "English",
+	["com.tencent.xinWeChat"] = "Chinese",
+	["com.tencent.WeWorkMac"] = "Chinese",
 }
 
 local function Chinese() -- 中文
-  hs.keycodes.currentSourceID("im.rime.inputmethod.Squirrel.Hans")
+	local flag = hs.keycodes.currentSourceID("im.rime.inputmethod.Squirrel.Hans")
+	if not flag then
+		hs.keycodes.currentSourceID("com.apple.inputmethod.SCIM.ITABC")
+	end
 end
 
 local function English() -- 英文
-  hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
-end
-
-local function updateFocusAppInputMethod(app)
-  -- app = hs.window.frontmostWindow():application()
-  local cur_input = hs.keycodes.currentSourceID()
-  local app_path = app:path()
-  local next_input = App2Ime[app_path]
-  if cur_input == next_input then
-    return
-  elseif next_input == "Chinese" then
-    Chinese()
-  else
-    English()
-  end
+	hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
 end
 
 local function applicationWatcher(appName, eventType, appObject)
-  if eventType == hs.application.watcher.activated or eventType == hs.application.watcher.launched then -- 切换窗口或首次启动时
-    updateFocusAppInputMethod(appObject)
-  end
+	if eventType == hs.application.watcher.activated or eventType == hs.application.watcher.launched then -- 切换窗口或首次启动时
+		local input = App2Ime[appObject:bundleID()]
+		if input then
+			if input == "Chinese" then
+				Chinese()
+			else
+				English()
+			end
+		end
+	end
 end
 
 appWatcher = hs.application.watcher.new(applicationWatcher)
