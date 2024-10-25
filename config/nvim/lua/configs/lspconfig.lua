1f65ddcf9c5f0804c58ff6c743ee332a46520108
hubogle <hubogle@gmail.com>
2024-10-25 22:30:37 +0800

    fix: vim lsp document highlight

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -1,28 +1,61 @@
 -- load defaults i.e lua_lsp
 dofile(vim.g.base46_cache .. "lsp")
-require("nvchad.configs.lspconfig").defaults()
-
 local lspconfig = require "lspconfig"
+local map = vim.keymap.set
+
+-- export on_attach & capabilities
+local on_attach = function(client, bufnr)
+    local function opts(desc)
+        return { buffer = bufnr, desc = "LSP " .. desc }
+    end
+
+    map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
+    map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
+    map("n", "gi", vim.lsp.buf.implementation, opts "Go to implementation")
+    map("n", "<leader>sh", vim.lsp.buf.signature_help, opts "Show signature help")
+    map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
+    map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
+
+    map("n", "<leader>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts "List workspace folders")
+
+    map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
+    map("n", "<leader>ra", require "nvchad.lsp.renamer", opts "NvRenamer")
+
+    map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
+    map("n", "gr", vim.lsp.buf.references, opts "Show references")
+
+    if client.supports_method "textDocument/documentHighlight" then
+        local group_name = "lsp_document_highlight"
+        vim.api.nvim_create_augroup(group_name, { clear = true })
+        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+            buffer = bufnr,
+            group = group_name,
+            callback = vim.lsp.buf.document_highlight,
+        })
+        vim.api.nvim_create_autocmd("CursorMoved", {
+            buffer = bufnr,
+            group = group_name,
+            callback = vim.lsp.buf.clear_references,
+        })
+    end
+end
 
 -- EXAMPLE
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.m
 local servers = { "gopls", "pylsp", "lua_ls", "bashls", "yamlls", "jsonls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
-        on_attach = nvlsp.on_attach,
+        on_attach = on_attach,
         on_init = nvlsp.on_init,
         capabilities = nvlsp.capabilities,
     }
 end
 
--- configuring single server, example: typescript
--- lspconfig.tsserver.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
 lspconfig["lua_ls"].setup {
     settings = {
         Lua = {
@@ -53,6 +86,8 @@ lspconfig["lua_ls"].setup {
         },
     },
 }
+
+require("nvchad.lsp").diagnostic_config()
 
 local x = vim.diagnostic.severity
 vim.diagnostic.config {
