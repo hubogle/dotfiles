541ef76a8348563a82c9f2d000efcc33889e02ac
hubogle <hubogle@gmail.com>
2025-09-24 16:36:38 +0800

    fix: vim update 0.11

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,5 @@
 -- load defaults i.e lua_lsp
 dofile(vim.g.base46_cache .. "lsp")
-local lspconfig = require "lspconfig"
 local map = vim.keymap.set
 
 -- export on_attach & capabilities
@@ -26,7 +25,7 @@ local on_attach = function(client, bufnr)
     map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
     map("n", "gr", vim.lsp.buf.references, opts "Show references")
 
-    if client.supports_method "textDocument/documentHighlight" then
+    if client:supports_method "textDocument/documentHighlight" then
         local group_name = "lsp_document_highlight"
         vim.api.nvim_create_augroup(group_name, { clear = true })
         vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
@@ -51,48 +50,53 @@ end
 local servers = { "gopls", "pylsp", "lua_ls", "bashls", "yamlls", "jsonls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-        on_attach = on_attach,
-        on_init = nvlsp.on_init,
-        capabilities = nvlsp.capabilities,
-    }
-end
+vim.lsp.enable(servers)
 
-lspconfig["lua_ls"].setup {
+local defaults = {
     on_attach = on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-    settings = {
-        Lua = {
-            runtime = {
-                version = "LuaJIT",
-            },
-            diagnostics = {
-                globals = { "use", "vim" },
-                disable = { "different-requires" },
-            },
-            hint = {
-                enable = true,
-                setType = true,
-            },
-            telemetry = {
-                enable = false,
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-                    [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-                    [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
-                    [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
+}
+
+for _, name in ipairs(servers) do
+    vim.lsp.config(name, defaults)
+    vim.lsp.enable(name)
+end
+
+vim.lsp.config(
+    "lua_ls",
+    vim.tbl_deep_extend("force", defaults, {
+        settings = {
+            Lua = {
+                runtime = {
+                    version = "LuaJIT",
                 },
-                maxPreload = 100000,
-                preloadFileSize = 10000,
+                diagnostics = {
+                    globals = { "use", "vim" },
+                    disable = { "different-requires" },
+                },
+                hint = {
+                    enable = true,
+                    setType = true,
+                },
+                telemetry = {
+                    enable = false,
+                },
+                workspace = {
+                    library = {
+                        [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+                        [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+                        [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
+                        [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
+                    },
+                    maxPreload = 100000,
+                    preloadFileSize = 10000,
+                },
             },
         },
-    },
-}
+    })
+)
+vim.lsp.enable "lua_ls"
 
 local diagnostics = require "configs.diagnostic"
 diagnostics.setup()
